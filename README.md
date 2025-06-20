c34ea1b2e9e470017478e0c06e09a78a6eb9e2a7
nikitaa-sharma <nikitaaasharma29@gmail.com>
2025-06-20 23:21:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Echo
+Its a music player, My own style music player.
+Developed in JAVA, used JLayer(javazoom) library for playing mp3.
+
---
+++ src/dev/nikita/echo/Launcher.java
@@ -0,0 +1,10 @@
+package dev.nikita.echo;
+
+public class Launcher 
+{
+	public static void main(String[] ar)
+	{
+		Animation objAnimation = new Animation();
+		objAnimation.animate();
+	}
+}
